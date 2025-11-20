3576d85f5561ef883ba9d2d8fcadaa5a6f117bd2
Medha Pant <medhapant4@gmail.com>
2025-11-20 12:12:01 +0530

    added logic for google  oauth

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -3,10 +3,6 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-
-import axios from "axios"
-import { useNavigate } from "react-router-dom"
-
 export  function Signup({ setIsLoggedIn, setUserRole }) {
 
   const navigate = useNavigate();
